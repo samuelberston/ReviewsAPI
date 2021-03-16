62464b804232ef0bfcd12b200d0104292db1236a
Sam Berston <samuelberston@gmail.com>
2021-03-16 12:32:04 -0700

    review routes

--- ReviewsAPI/MongoDB/index.js
+++ ReviewsAPI/MongoDB/index.js
@@ -6,6 +6,10 @@ db.once('open', function(){
   console.log(Connected to MongoDB successfully!);
 });
 
+let product = mongoose.Schema({
+  product_id: Number,
+});
+
 let reviewSchema = mongoose.Schema({
   review_id: Number,
   rating: Number,
@@ -19,51 +23,13 @@ let reviewSchema = mongoose.Schema({
   photos: Array,
 });
 
-let Review = mongoose.model('Review', repoSchema);
+let photoSchema = mongoose.Schema({
+  photo_id: Number,
+  photo_url: String,
+  review_id: Number,
+});
 
-let saveReview = (reviewData) => {
-  reviewData.forEach(review => {
-    const newReview = new Review({
-      review_id: review.review_id,
-      rating: review.rating,
-      summary: review.summary,
-      recommend: review.recommend,
-      response: review.response,
-      body: review.body,
-      date: review.date,
-      reviewer_name: review.reviewer_name,
-      helpfulness: review.helpfulness,
-      photos: review.photos,
-    });
-
-    newReview.save(function(err, newReview) {
-      if (err) { console.error(err); }
-      console.log(newReview);
-    })
-  })
-}
-
-let metaSchema = mongoose.Schema({
-  product_id: String,
-  ratings: Object,
-  recommended: Object,
-  characteristics: Object,
-})
-
-let Meta = mongoose.model('Meta', metaSchema);
-
-let saveMeta = (meta) => {
-  const newMeta = new Meta({
-    product_id: meta.product_id,
-    ratings: meta.ratings,
-    recommended: meta.recommended,
-    characteristics: meta.characteristics,
-  })
-}
-
-module.exports = {
-  Review,
-  saveReview,
-  Meta,
-  saveMeta,
-};
+let characteristicSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+});
