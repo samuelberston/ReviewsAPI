616ef41b9530a27f0df7ed2d2b756ebd10715ddd
Sam Berston <samuelberston@gmail.com>
2021-03-16 17:36:17 -0700

    CSV files and schema.sql moved

--- ReviewsAPI/schema.sql
+++ ReviewsAPI/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS reviews;
+
 CREATE DATABASE reviews;
 
 -- send API request to products, integrate with Nils
@@ -7,45 +9,47 @@ CREATE TABLE products (
 
 CREATE TABLE reviews (
   review_id integer PRIMARY KEY,
-  rating int NOT NULL,
-  summary VARCHAR NOT NULL,
-  recommend bit NOT NULL,
-  response VARCHAR NULL,
-  body VARCHAR NOT NULL,
-  date DATETIME NOT NULL,
-  reviewer_name VARCHAR NOT NULL,
-  helpfulness INT NOT NULL,
   product_id INT NOT NULL,
+  rating int NOT NULL,
+  date DATETIME NOT NULL,
+  summary VARCHAR NOT NULL,
+  body VARCHAR NOT NULL,
+  recommend bit NOT NULL,
+  reported bit NOT NULL,
+  response VARCHAR NULL,
+  reviewer_name VARCHAR NOT NULL,
+  reviewer_email VARCHAR NOT NULL,
+  response VARCHAR NULL,
+  helpfulness INT NOT NULL,
   FOREIGN KEY (product_id) REFERENCES products(product_id)
 )
 
 CREATE TABLE photos (
   photo_id int NOT NULL,
-  photo_url int NOT NULL,
-  product_id int NOT NULL,
   review_id integer NOT NULL,
-  FOREIGN KEY (product_id) REFERENCES products(product_id), -- some photos might have the same review_id but different product
+  photo_url int NOT NULL,
   FOREIGN KEY (review_id) REFERENCES reviews(review_id),
 )
 
 CREATE TABLE characteristics (
   characteristic_id integer NOT NULL PRIMARY KEY,
+  product_id int NOT NULL,
   characteristic_name VARCHAR NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES products(product_id),
 )
 
-CREATE TABLE product_characteristics (
+CREATE TABLE characteristics_reviews (
   id int NOT NULL PRIMARY KEY,
-  product_id int NOT NULL,
+  review_id integer NOT NULL,
   characteristic_id integer NOT NULL,
   characteristic_value INT NOT NULL,
-  FOREIGN KEY product_id REFERENCES products(product_id),
-  FOREIGN KEY characteristic_id REFERENCES characteristics(characteristic_id),
-)
-
-CREATE TABLE reviews_characteristics (
-  product_characteristics_id int NOT NULL
-  review_id integer NOT NULL
-  review_value INT NOT NULL,
-  FOREIGN KEY(product_characteristics_id) REFERENCES product_characteristics(id)
+  FOREIGN KEY(product_characteristics_id) REFERENCES product_characteristics(id),
   FOREIGN KEY (review_id) REFERENCES reviews(review_id),
 )
+
+LOAD DATA INFILE '../../CSV/reviews.csv'
+INTO TABLE reviews
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
