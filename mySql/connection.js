e315cfa5e72e3f460f5b5273c61be3ba85d229bc
Sam Berston <samuelberston@gmail.com>
2021-03-17 22:42:57 -0700

    airbnb linter

--- mySql/connection.js
+++ mySql/connection.js
@@ -1,15 +1,15 @@
-var mysql = require('mysql2');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'reviewsAPI',
-})
+});
 
 connection.connect((err) => {
   if (err) { console.log(err); }
   console.log('connected to mysql');
-})
+});
 
 module.exports = connection;
