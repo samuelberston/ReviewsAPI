a1b2b087f6c095f3467904bf9dbbe7edf535a60b
Sam Berston <samuelberston@gmail.com>
2021-03-22 14:51:43 -0700

    ETL scripts

--- seed.sql
+++ seed.sql
@@ -1,24 +1,24 @@
 USE reviewsAPI;
 
-LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/reviews.csv'
+LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/reviews-clean.csv'
 INTO TABLE reviews
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/reviewsPhotos.csv'
+LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/reviewsPhotos-clean.csv'
 INTO TABLE photos
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/characteristics.csv'
+LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/characteristics-clean.csv'
 INTO TABLE characteristics
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/characteristicsReviews.csv'
+LOAD DATA LOCAL INFILE '/Users/samuelberston/Desktop/CSV/characteristicsReviews-clean.csv'
 INTO TABLE characteristics_reviews
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
