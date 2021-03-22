276342f9f3ece406d8e9856d4bea071d14cddbfe
Sam Berston <samuelberston@gmail.com>
2021-03-22 15:01:35 -0700

    removed console logs

--- server/reviewRoutes.js
+++ server/reviewRoutes.js
@@ -6,7 +6,7 @@ reviewRoutes.get('/reviews', (req, res) => {
   const productId = req.query.product_id;
 
   connection.query(`SELECT * FROM reviews WHERE product_id = '${productId}'`, (err, result) => {
-    if (err) { console.log(err); }
+    if (err) { throw err; }
     res.send(result);
   });
 });
@@ -15,7 +15,7 @@ reviewRoutes.get('/reviews/photos', (req, res) => {
   const productId = req.query.product_id;
 
   connection.query(`SELECT photo_id, photo_url FROM photos INNER JOIN reviews ON photos.review_id = reviews.review_id AND reviews.product_id = '${productId}'`, (err, result) => {
-    if (err) { console.log(err); }
+    if (err) { throw err; }
     res.send(result);
   });
 });
@@ -24,7 +24,7 @@ reviewRoutes.get('/reviews/meta/ratings', (req, res) => {
   const productId = req.query.product_id;
 
   connection.query(`SELECT rating, COUNT(rating) FROM reviews WHERE product_id = ${productId} GROUP BY rating`, (err, data) => {
-    if (err) { console.log(err); }
+    if (err) { throw err; }
     const ratings = {};
     data.forEach((rating) => {
       ratings[rating.rating] = rating['COUNT(rating)'];
@@ -37,10 +37,10 @@ reviewRoutes.get('/reviews/meta/recommend', (req, res) => {
   const productId = req.query.product_id;
 
   connection.query(`SELECT COUNT(recommend) FROM reviews WHERE recommend = true AND product_id = ${productId}`, (err, data) => {
-    if (err) { console.log(err); }
+    if (err) { throw err; }
     const t = data[0]['COUNT(recommend)'];
     connection.query(`SELECT COUNT(recommend) FROM reviews WHERE recommend = false AND product_id = ${productId}`, (err1, data1) => {
-      if (err1) { console.log(err1); }
+      if (err1) { throw err; }
       const f = data1[0]['COUNT(recommend)'];
       const recommend = {
         true: t,
@@ -62,8 +62,7 @@ reviewRoutes.get('/reviews/meta/characteristics', (req, res) => {
 
 reviewRoutes.post('/reviews', (req, res) => {
   connection.query(`INSERT INTO reviews (rating, summary, recommend, body, reviewer_name, product_id) VALUES (${req.body.rating}, ${req.body.summary}, ${req.body.recommend}, ${req.body.body}, ${req.body.reviewer_name}, ${req.body.reviewer_email}, ${req.body.product_id})`, (err, data) => {
-    if (err) { console.log(err); }
-    console.log('posted review');
+    if (err) { throw err; }
     res.status(201).send(data);
   });
   // and the photos
@@ -74,8 +73,7 @@ reviewRoutes.put('/helpful', (req, res) => {
   const reviewId = req.query.review_id;
 
   connection.query(`UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id = ${reviewId}`, (err, data) => {
-    if (err) { console.log(err); }
-    console.log('marked helpful');
+    if (err) { throw err; }
     res.status(201).send(data);
   });
 });
@@ -85,8 +83,7 @@ reviewRoutes.put('/report', (req, res) => {
   const reviewId = req.query.review_id;
 
   connection.query(`UPDATE reviews SET reported = '1' WHERE review_id = ${reviewId}`, (err, data) => {
-    if (err) { console.log(err); }
-    console.log('reported');
+    if (err) { throw err; }
     res.status(201).send(data);
   });
 });
