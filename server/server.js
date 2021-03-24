0a6efc3da33c3fcec76682fd87ce68f126af5382
Sam Berston <samuelberston@gmail.com>
2021-03-23 23:14:27 -0700

    loaderio

--- server/server.js
+++ server/server.js
@@ -4,8 +4,13 @@ const express = require('express');
 const app = express();
 
 const reviewRoutes = require('./reviewRoutes.js');
+const loaderio = require('../loaderio-0026bfa91604c6a32449aa41c270528f.txt');
 
 app.use('/', reviewRoutes);
+
+app.get('/loaderio-0026bfa91604c6a32449aa41c270528f', (req, res) => {
+  res.render(loaderio);
+});
 
 app.get('/', (req, res) => {
   res.render('index');
